e2efb210567aa0c56827ee73ee01e28fd96530c1
jayshideler <jayshideler@yahoo.com>
2020-06-13 18:31:29 -0600

    Initial Commit

---
+++ ExampleUIKItPreview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleUIKItPreview
+//
+//  Created by Jay Randall Shideler on 6/13/20.
+//  Copyright © 2020 Jay Randall Shideler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
